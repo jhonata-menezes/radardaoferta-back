c8c744b2029e78a28cb2e4e4bf7c6a01d250520e
Jhonata Menezes <jhonata.menezes@forseti.com.br>
2016-11-24 16:48:23 -0200

    nova api da netshoes

--- netshoes.go
+++ netshoes.go
@@ -6,26 +6,21 @@ import (
 	"log"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 type NetshoesProduto struct {
-	ID      string
+	Name    string
+	ID      string `json: "base_sku"`
 	Link    string
 	Message string
 	Value   struct {
-		Ecommerce struct {
-			Detail struct {
-				Products []struct {
-					Id          string
-					price       string
-					Dimension30 string
-				}
-			}
+		Price struct {
+			Atual string `json:"actual_price"`
 		}
-		Chaordic struct {
-			Product struct {
-				Name  string
-				Price float32
+		Gallery []struct {
+			Items []struct {
+				Large string
 			}
 		}
 	}
@@ -37,10 +32,12 @@ func NetshoesParse(url string) (ProdutoGenerico, error) {
 		log.Println(err)
 		return ProdutoGenerico{}, errors.New("nao identificou id da api netshoes")
 	}
-	urlApi := NetshoesIdentificaUrlApi(idUrl)
-
+	urlApi := NetshoesMountUrl(idUrl)
 	produto := NetshoesProduto{}
 	Request(urlApi, &produto)
+	if produto.Message != "success" {
+		return ProdutoGenerico{}, errors.New("api da netshoes nao retornou dados")
+	}
 	produto.ID = idUrl
 	produto.Link = url
 
@@ -49,7 +46,7 @@ func NetshoesParse(url string) (ProdutoGenerico, error) {
 
 func NetshoesIdentificaIdApi(url string) (string, error) {
 	//http://www.netshoes.com.br/produto/chuteira-adidas-artilheira-in-futsal-D13-3054-244?&lkey=a2
-	r, err := regexp.Compile("(\\w{2,5}\\-\\w{2,5}\\-\\w{2,5})\\?")
+	r, err := regexp.Compile("(\\w{2,5}\\-\\w{2,5}\\-\\w{2,5})(\\?|$)")
 	if err != nil {
 		panic(err)
 	}
@@ -60,45 +57,22 @@ func NetshoesIdentificaIdApi(url string) (string, error) {
 	return idUrl[1], nil
 }
 
-func NetshoesIdentificaUrlApi(id string) string {
-	for i := 50; i >= 0; i-- {
-		produto := NetshoesProduto{}
-		url := NetshoesMountUrl(id, strconv.Itoa(i))
-		Request(url, &produto)
-		if produto.Message == "success" {
-			return url
-		}
-	}
-	return ""
-	//http://www.netshoes.com.br/services/analytics-product-data.jsp?productId=D13-3054&skuId=D13-3054-244-44
-
-}
-
-func NetshoesMountUrl(idProduto string, idTentativa string) string {
-	r, err := regexp.Compile("(\\w{2,5}\\-\\w{2,5})\\-")
-	if err != nil {
-		panic(err)
-	}
-	subId := r.FindStringSubmatch(idProduto)
-	return fmt.Sprintf("http://www.netshoes.com.br/services/analytics-product-data.jsp?productId=%s&skuId=%s-%s",
-		subId[1],
-		idProduto,
-		idTentativa)
-}
-
-func NetshoesImagem(idUrl string) string {
-	//http://static1.netshoes.net/Produtos/chuteira-adidas-artilheira-in-futsal/44/D13-3054-244/D13-3054-244_zoom1.jpg?resize=544:*
-	return fmt.Sprintf("http://static1.netshoes.net/Produtos/chuteira-adidas-artilheira-in-futsal/%s/%s/%s_zoom1.jpg?resize=400:*", idUrl[len(idUrl)-2:], idUrl, idUrl)
+func NetshoesMountUrl(idProduto string) string {
+	return fmt.Sprintf("http://www.netshoes.com.br/services/get-complete-product-vo.jsp?_dyncharset=utf-8&productId=%s", idProduto)
 }
 
 func NetshoesDeParaGenerico(p NetshoesProduto) ProdutoGenerico {
 	g := ProdutoGenerico{}
 	g.Link = p.Link
 	g.IDProduto = p.ID
-	g.Nome = p.Value.Chaordic.Product.Name
-	g.Valor = p.Value.Chaordic.Product.Price
-	g.Imagens = []string{NetshoesImagem(p.ID)}
+	g.Nome = p.Name
+	g.Imagens = []string{"http://" + p.Value.Gallery[0].Items[0].Large[2:]}
 	g.Created = TimeNowIso()
 	g.Loja = "netshoes"
+	preco, err := strconv.ParseFloat(
+		strings.Replace(p.Value.Price.Atual[3:], ",", ".", 1), 32)
+	if err == nil {
+		g.Valor = float32(preco)
+	}
 	return g
 }
