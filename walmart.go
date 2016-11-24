df29b6f812ff9aa1bd812316934b1c838c103b86
Jhonata Menezes <jhonatamenezes10@gmail.com>
2016-11-24 21:22:06 -0200

    integracao com walmart ok

--- walmart.go
+++ walmart.go
@@ -3,7 +3,6 @@ package sopromocao
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
@@ -20,8 +19,12 @@ type WalmartProduto struct {
 }
 
 func WalmartParse(url string) (ProdutoGenerico, error) {
-	WalmartGetInfo(url)
-	return ProdutoGenerico{}, errors.New("err")
+	produto, err := WalmartGetInfo(url)
+	if err != nil {
+		return ProdutoGenerico{}, err
+	}
+	produto.Link = url
+	return WalmartDePara(produto), nil
 }
 
 func WalmartGetInfo(url string) (WalmartProduto, error) {
@@ -29,7 +32,6 @@ func WalmartGetInfo(url string) (WalmartProduto, error) {
 	body := string(RequestBody(url))
 	r, _ := regexp.Compile("\\<script\\>var dataLayer \\= \\[(.*?)\\]\\;dataLayer\\.push")
 	match := r.FindStringSubmatch(string(body))
-	fmt.Printf("%#v", match)
 	if len(match) >= 2 {
 		produto := WalmartProduto{}
 		err := json.Unmarshal([]byte(match[1]), &produto)
@@ -43,14 +45,17 @@ func WalmartGetInfo(url string) (WalmartProduto, error) {
 
 func WalmartDePara(w WalmartProduto) ProdutoGenerico {
 	p := ProdutoGenerico{}
-	p.IDProduto = w.Product[0].ID
+	p.IDProduto = strconv.Itoa(w.Product[0].ID)
 	p.Nome = w.Product[0].Nome
 	p.Created = TimeNowIso()
 	p.Loja = "walmart"
 
-	valor64, err := strconv.ParseFloat(p.Valor, 32)
+	valor64, err := strconv.ParseFloat(w.Product[0].Valor, 32)
 	if err == nil {
 		p.Valor = float32(valor64)
 	}
+	imagem := "http://" + strings.Replace(w.Product[0].Imagens[2:], ".jpg", "/a.jpg", 1)
+	p.Imagens = []string{imagem}
+	p.Link = strings.Replace(w.Link, "://", "://www.", 1)
 	return p
 }
